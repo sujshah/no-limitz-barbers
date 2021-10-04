1738871bcd2488e7dd83212284d29ea237073a36
Suraj Shah <suraj1.shah@gmail.com>
2021-10-04 21:45:47 +0100

    add pricing section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,5 @@
 import * as React from 'react'
 
-const backgroundStyles = {
-  backgroundColor: "#212121",
-}
-
 const pageStyles = {
   color: "#FFFFFF",
   fontFamily: "Stencil Std",
@@ -14,19 +10,35 @@ const pageStyles = {
   padding: "560px",
   border: "none",
   fontSize: "26px",
+  backgroundColor: "#212121",
+}
+
+const pricingStyles = {
+  color: "#FFFFFF",
+  fontFamily: "Stencil Std",
+  textAlign: "center",
+  maxWidth: "none ",
+  transition: "none",
+  margin: "none",
+  padding: "560px",
+  border: "none",
+  fontSize: "26px",
+  backgroundColor: "#000000",
 }
 
 const IndexPage = () => {
   return (
-    <body style={backgroundStyles}>
+    <body>
     <main style={pageStyles}>
       <title>
         Home Page
       </title>
       <h1>NO LIMITZ BARBERS</h1>
     </main>
+    <section style={pricingStyles}>
+      <h1>PRICING</h1>
+    </section>
     </body>
-    
   )
 }
 
