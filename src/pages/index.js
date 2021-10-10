f56e17a1d59eb4d4376f510caa79999fdc4d809c
Suraj Shah <suraj1.shah@gmail.com>
2021-10-10 22:00:05 +0100

    add title image

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const pageStyles = {
   margin: "none",
   padding: "560px",
   border: "none",
-  fontSize: "26px",
+  fontSize: "40px",
   backgroundColor: "#212121",
 }
 
@@ -23,7 +23,13 @@ const pricingStyles = {
   padding: "560px",
   border: "none",
   fontSize: "26px",
-  backgroundColor: "#000000",
+  backgroundColor: "#212121",
+}
+
+const imgStyles = {
+  width: "1000px",
+  height: "auto",
+  objectFit: "cover",
 }
 
 const IndexPage = () => {
@@ -36,7 +42,7 @@ const IndexPage = () => {
     </head>
     <body>
     <main style={pageStyles}>
-      <h1>NO LIMITZ BARBERS</h1>
+      <h1><img style={imgStyles} src="https://fontmeme.com/permalink/211010/f0cb3d7a35da93e6a3ea024a6996c7a3.png" alt="graffiti-creator" border="0"></img></h1>
     </main>
     <section style={pricingStyles}>
       <h1>PRICING</h1>
